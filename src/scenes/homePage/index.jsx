321bde550aaf29c432b99e93718493f1e71b15a0
vratant11 <vratant2012061@akgec.ac.in>
2023-02-04 17:30:36 +0530

    Navbar Created

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,6 +1,9 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
 const HomePage =()=>{
     return(
-        <div>Homepage</div>
+        <Box><Navbar/></Box>
     )
 };
 
--- src/theme.js
+++ src/theme.js
@@ -105,20 +105,28 @@ export const colorTokens = {
         fontFamily: ["Rubik","sans-serif"].join(","),
         fontSize: 12,
         h1:{
-            fontFamily:["Rubik","sans-serif"],
+            fontFamily:["Rubik","sans-serif"].join(","),
             fontSize: 40,
         },
         h2:{
-            fontFamily:["Rubik","sans-serif"],
+            fontFamily:["Rubik","sans-serif"].join(","),
             fontSize: 32,
         },
         h3:{
-            fontFamily:["Rubik","sans-serif"],
+            fontFamily:["Rubik","sans-serif"].join(","),
             fontSize: 24,
         },
-        h4:{
-            fontFamily:["Rubik","sans-serif"],
+        h5:{
+            fontFamily:["Rubik","sans-serif"].join(","),
             fontSize: 20,
+        },
+        h4:{
+            fontFamily:["Rubik","sans-serif"].join(","),
+            fontSize: 16,
+        },
+        h4:{
+            fontFamily:["Rubik","sans-serif"].join(","),
+            fontSize: 14,
         },
       }
     };
